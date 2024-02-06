2d3caed2aafca2ba8b478d4a1908b5b9e4944185
Deepesh Gotia <deepeshgotiya2000@gmail.com>
2024-02-06 23:14:47 +0530

    wIP

--- mypf1/src/MyNavbar.jsx
+++ mypf1/src/MyNavbar.jsx
@@ -1,23 +1,35 @@
+// Import necessary components and modules
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
+import AboutMe from "./AboutMe"; // Import your About component
+import LandingPage from "./LandingPage";
 
+// Navbar component
 function MyNavbar() {
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Deepesh Gotia</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#resume">Resume</Nav.Link>
-            <Nav.Link href="#technologies">Technologies</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Router>
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Deepesh Gotia</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/resume">Resume</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Routes>
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/#home" element={<LandingPage />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
